a79c2371fc1d07b4e9d62dd81be715f870026090
vardhan-rawat <vardhanrawat1@gmail.com>
2023-11-23 16:33:19 +0530

    update for deployment

--- youtubedown/settings.py
+++ youtubedown/settings.py
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'youtubedown.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 
@@ -116,9 +116,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATICFILES_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
+
 STATIC_URL = 'static/'
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 
 # Default primary key field type
